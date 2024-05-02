1bd8a742ef34e288e31e46d98eb2e18540d89f06
rastridge <rfa@me.com>
2024-05-02 09:01:39 -0400

    correct use of vue-chartjs

--- components/displays/RosterChart.vue
+++ components/displays/RosterChart.vue
@@ -9,6 +9,24 @@
 
 <script setup>
 	import { Bar } from 'vue-chartjs'
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale,
+	} from 'chart.js'
+
+	ChartJS.register(
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale
+	)
 
 	const chartData = ref()
 	const chartOptions = ref()

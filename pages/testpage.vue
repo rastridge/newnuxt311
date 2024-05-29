ceb18f9406260ce006058e07394e22262619cb92
rastridge <rfa@me.com>
2024-05-28 21:50:37 -0400

    hello there 2

--- pages/testpage.vue
+++ pages/testpage.vue
@@ -5,7 +5,7 @@ const title = ref('originaltitle.value')
 </script>
 
 <template>
-	<p>Hello there Hello there</p>
+	<p>Hello there Hello there Hello there Hello there</p>
 	<!-- <testvmodel :modelValue="title" @update:modelValue="$event => (title = $event) " /> -->
 	<testvmodel v-model="title" />
 	<br>

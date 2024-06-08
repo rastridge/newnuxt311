3e581b3900b3d06a6e29086adaaa624a90ec9c90
rastridge <rfa@me.com>
2024-06-07 20:07:21 -0400

    formatting?

--- pages/loginpage.vue
+++ pages/loginpage.vue
@@ -7,35 +7,19 @@
 				</div>
 				<FormKit type="form" submit-label="Login" @submit="handleSubmit">
 					<display-alert />
-					<FormKit
-						type="text"
-						name="username"
-						label="Username"
-						validation="length:3"
-						validation-visibility="live"
+					<FormKit type="text" name="username" label="Username" validation="length:3" validation-visibility="live"
 						:validation-messages="{
 							matches: 'Username must be at least 3 chars',
-						}"
-					/>
+						}" />
 
-					<FormKit
-						type="password"
-						name="password"
-						label="Password"
-						validation="length:5"
-						validation-visibility="live"
+					<FormKit type="password" name="password" label="Password" validation="length:5" validation-visibility="live"
 						:validation-messages="{
 							matches: 'Password must be at least 5 chars',
-						}"
-					/>
+						}" />
 
 					<div class="flex align-items-center justify-content-between mb-6">
-						<nuxt-link
-							to="/resetpassword"
-							class="text-sm no-underline ml-2 text-blue-500 text-right cursor-pointer"
-							active-class="active"
-							><a>Forgot password?</a></nuxt-link
-						>
+						<nuxt-link to="/resetpassword" class="text-sm no-underline ml-2 text-blue-500 text-right cursor-pointer"
+							active-class="active"><a>Forgot password?</a></nuxt-link>
 					</div>
 				</FormKit>
 				<display-cancelform />
@@ -45,12 +29,12 @@
 </template>
 
 <script setup>
-	import { useAuthStore } from '~/stores/authStore'
-	const auth = useAuthStore()
+import { useAuthStore } from '~/stores/authStore'
+const auth = useAuthStore()
 
-	const handleSubmit = async (state) => {
-		if (state.username && state.password) {
-			auth.login(state.username, state.password) // pinia auth store
-		}
+const handleSubmit = async (state) => {
+	if (state.username && state.password) {
+		auth.login(state.username, state.password) // pinia auth store
 	}
+}
 </script>

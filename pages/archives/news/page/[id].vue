41cc67f32f53d0698bb32f3f599ad9804e3c2afe
rastridge <rfa@me.com>
2024-06-13 14:00:16 -0400

    change page bkgrd/color

--- pages/archives/news/page/[id].vue
+++ pages/archives/news/page/[id].vue
@@ -1,13 +1,12 @@
 <template>
 	<div>
+
 		<Head>
 			<Title>Buffalo Rugby Club {{ news_data.news_title }}</Title>
 		</Head>
 		<Common-header :title="news_data.news_title" />
 		<div class="my-simple-card-style">
-			<div
-				class="my-text-style border-round-xl md:shadow-6 m-1 p-2 md:m-2 md:p-3 bg-blue-600 text-white"
-			>
+			<div class="my-text-style border-round-xl md:shadow-6 m-1 p-2 md:m-2 md:p-3">
 				<span v-html="news_data.news_article"></span>
 			</div>
 		</div>
@@ -15,15 +14,15 @@
 </template>
 
 <script setup>
-	//
-	// Get content id parameter
-	//
-	const route = useRoute()
-	const news_id = ref(route.params.id)
-	//
-	// Get custom page content
-	//
-	const { data: news_data } = await useFetch(`/news/${news_id.value}`, {
-		method: 'get',
-	})
+//
+// Get content id parameter
+//
+const route = useRoute()
+const news_id = ref(route.params.id)
+//
+// Get custom page content
+//
+const { data: news_data } = await useFetch(`/news/${news_id.value}`, {
+	method: 'get',
+})
 </script>
